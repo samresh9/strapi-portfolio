a91211bbb5a2632b77bef3762585612ae6f80207
Samresh <thapasamresh9@gmail.com>
2024-03-28 20:54:35 +0545

    inital commit

---
+++ src/api/resturnat/controllers/resturnat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resturnat controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::resturnat.resturnat');
---
+++ src/api/resturnat/routes/resturnat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resturnat router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::resturnat.resturnat');
